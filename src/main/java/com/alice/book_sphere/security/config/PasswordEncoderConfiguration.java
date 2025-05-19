1ebfa9b714a20c50098eb38eb716d04f5c3e4b55
Alice <zxa2484@gmail.com>
2025-05-19 22:24:13 +0300

    add PasswordEncoderConfiguration

---
+++ src/main/java/com/alice/book_sphere/security/config/PasswordEncoderConfiguration.java
@@ -0,0 +1,14 @@
+package com.alice.book_sphere.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
