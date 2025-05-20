b3ae356ef96a66d9c5a068b1869d7f7bdf1f7e00
Alice <zxa2484@gmail.com>
2025-05-20 16:08:18 +0300

    delete @CacheConfig annotation

--- src/main/java/com/alice/book_sphere/repository/UserRepository.java
+++ src/main/java/com/alice/book_sphere/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
-//@CacheConfig(cacheNames = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
